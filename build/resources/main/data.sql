183e3bf0536fbc598273eecf55962f507a72eff8
Yevgenii Krasnenkov <ykrasnenkov@corelogic.com>
2023-04-16 11:25:02 -0400

    init commit

--- build/resources/main/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO _user (email, firstname, lastname, password, role)
-VALUES
-    ('john@evgen.com', 'John', 'Doe', '$2a$12$OBW7UwA9fC/f/oHR4pvG1.nqkjvTiH5C7hskLaTiuet7/Vdi/Clti', 'USER'),
-    ('jane@evgen.com', 'Jane', 'Doe', 'password2', 'ADMIN'),
-    ('bob@evgen.com', 'Bob', 'Smith', 'password3', 'USER'),
-    ('alice@evgen.com', 'Alice', 'Smith', 'password4', 'USER'),
-    ('david@evgen.com', 'David', 'Lee', 'password5', 'ADMIN');
-
